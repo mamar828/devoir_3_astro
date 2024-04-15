28f09ffc69270dfe90614abf7019f38d470ddc23
Mathieu Marquis <mamar828@ulaval.ca>
2024-04-15 13:27:40 -0400

    better graphs and simpler get_array

--- tools.py
+++ tools.py
@@ -3,28 +3,20 @@ import matplotlib.pyplot as plt
 
 
 def get_array(filename):
-    with open(filename, "r") as f:
-        lines = f.readlines()
-        
-    data = []
-    for line in lines:
-        splitted_line = line.split(" ")
-        data.append([float(value) for value in splitted_line if not value in ["", "\n"]])
-        
-    return np.array(data)
+    return np.loadtxt(filename)
 
 
 def plot_file(filename):
     data = get_array(filename)
     fig, ax = plt.subplots(1,1)
-    ax.plot(data[:,0], data[:,1], "r-")
-    ax.plot(data[:,0], data[:,2], "g--")
-    ax.plot(data[:,0], data[:,3], "b:")
+    ax.plot(np.log10(data[:,0]), data[:,1], "r-", label=r"$\alpha=2.35$, $M_{up}=100 M_\odot$")
+    ax.plot(np.log10(data[:,0]), data[:,2], "g--", label=r"$\alpha=3.30$, $M_{up}=100 M_\odot$")
+    ax.plot(np.log10(data[:,0]), data[:,3], "b:", label=r"$\alpha=2.35$, $M_{up}=30 M_\odot$")
 
     ax.invert_yaxis()
-    ax.set_xscale("log")
-    ax.set_xlim(10**6, 10**9)
+    ax.set_xlim(6, 9)
 
-    ax.set_xlabel("Time [yr]")
+    ax.set_xlabel("log (Time [yr])")
     ax.set_ylabel("Value")
+    plt.legend(loc="upper right")
     plt.show()
